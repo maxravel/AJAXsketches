f2e0c3da82967fec21112c86c7900f47e37babba
maxravel <maxmaterniak@gmail.com>
2018-03-20 23:26:56 +0100

    AJAX open, send, onload, onerror, onreadystatechange,

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
-window.onload = function(){
+// window.onload = function(){
+
+//     //vanilla js
+//     var http = new XMLHttpRequest();
+
+//     http.onreadystatechange = function(){
+//         if(http.readyState == 4 && http.status == 200){
+//             console.log(JSON.parse(http.response));
+//         }
+//     };
+
+//     http.open('GET', 'data/tasks.json', true);
+//     http.send();
+//     //console.log(http);
+
+//     //jQuery method
+//     // $.get('data/tasks.json', function(data){
+//     //     console.log(data);
+//     // });
+//     // console.log("test");
+
+//     //callbacks
+//     // var fruits = ['mango', 'avocado', 'apple'];
+//     // fruits.forEach(function(val){
+//     //     console.log(val);
+//     // });
+
+//     function callbackFunction(data){
+//         console.log(data);
+//     }
+//     $.get('data/tasks.json', callbackFunction);
+
+// };
 
 
-    //vanilla js
-    var http = new XMLHttpRequest();
+document.querySelector('#button').addEventListener('click', loadData);
 
-    http.onreadystatechange = function(){
-        if(http.readyState == 4 && http.status == 200){
-            // console.log(JSON.parse(http.response));
+
+function loadData(){
+
+    xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'data/tasks.json', true);
+
+    // xhr.onreadystatechange = function(){
+    //     console.log(xhr.readyState);
+    // }
+
+    //optional for loaders/spinners
+    xhr.onprogress = function(){
+        console.log(xhr.readyState);
+    }
+
+    xhr.onload = function(){
+        console.log(xhr.readyState);
+        if(this.status === 200){
+            console.log(this.responseText);
+            document.querySelector('#output').innerHTML = this.responseText;
         }
-    };
+    }
 
-    http.open('GET', 'data/tasks.json', true);
-    http.send();
-    //console.log(http);
+    xhr.onerror = function(){
+        console.log('request error');
+    }
+    
+    xhr.send();
 
 
-    //jQuery method
-    $.get('data/tasks.json', function(data){
-        console.log(data);
-    });
-    console.log("test");
-
-};
+}
