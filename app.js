73d149b53efd303ef3b9e8621b27c2ea7154c188
maxravel <maxmaterniak@gmail.com>
2018-04-22 11:17:21 +0200

    external API

--- app.js
+++ app.js
@@ -35,7 +35,6 @@
 
 
 //*************************EXAMPLE ONE
-
 // document.querySelector('#button').addEventListener('click', loadData);
 
 // function loadData(){
@@ -72,98 +71,133 @@
 
 
 //*************************EXAMPLE TWO
+// document.querySelector('#btn1').addEventListener('click', loadCustomer);
+// function loadCustomer(e){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'data/customer.json', true);
+//     xhr.send();
+//     xhr.onload = function(){
+//         if(this.status === 200){
+//             // console.log(this.responseText);
 
-document.querySelector('#btn1').addEventListener('click', loadCustomer);
-function loadCustomer(e){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'data/customer.json', true);
-    xhr.send();
-    xhr.onload = function(){
-        if(this.status === 200){
-            // console.log(this.responseText);
+//             const customer = JSON.parse(this.responseText);
+//             // console.log(customer);
 
-            const customer = JSON.parse(this.responseText);
-            // console.log(customer);
-
-            const output = `
-            <ul>
-                <li>ID: ${customer.id}</li>
-                <li>Name: ${customer.name}</li>
-                <li>Company: ${customer.company}</li>
-                <li>Phone: ${customer.phone}</li>
-            </ul>
-            `
-            document.querySelector('#customer').innerHTML =  output;
-        }
-    }
-}
+//             const output = `
+//             <ul>
+//                 <li>ID: ${customer.id}</li>
+//                 <li>Name: ${customer.name}</li>
+//                 <li>Company: ${customer.company}</li>
+//                 <li>Phone: ${customer.phone}</li>
+//             </ul>
+//             `
+//             document.querySelector('#customer').innerHTML =  output;
+//         }
+//     }
+// }
 
 
-//CUSTOMERS***************************8
-document.querySelector('#btn2').addEventListener('click', loadCustomers);
-function loadCustomers(e){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'data/customers.json', true);
-    xhr.send();
-    xhr.onload = function(){
-        if(this.status === 200){
-            // console.log(this.responseText);
 
-            const customers = JSON.parse(this.responseText);
-            // console.log(customer);
+//**************************CUSTOMERS***********
+// document.querySelector('#btn2').addEventListener('click', loadCustomers);
+// function loadCustomers(e){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'data/customers.json', true);
+//     xhr.send();
+//     xhr.onload = function(){
+//         if(this.status === 200){
+//             // console.log(this.responseText);
 
-            let output ='' 
-            customers.forEach(function(customer){
-                output+=`
-                <ul>
-                    <li>ID: ${customer.id}</li>
-                    <li>Name: ${customer.name}</li>
-                    <li>Company: ${customer.company}</li>
-                    <li>Phone: ${customer.phone}</li>
-                </ul>
-                `
-            });
+//             const customers = JSON.parse(this.responseText);
+//             // console.log(customer);
+
+//             let output ='' 
+//             customers.forEach(function(customer){
+//                 output+=`
+//                 <ul>
+//                     <li>ID: ${customer.id}</li>
+//                     <li>Name: ${customer.name}</li>
+//                     <li>Company: ${customer.company}</li>
+//                     <li>Phone: ${customer.phone}</li>
+//                 </ul>
+//                 `
+//             });
             
-            document.querySelector('#customers').innerHTML =  output;
+//             document.querySelector('#customers').innerHTML =  output;
+//         }
+//     }
+// }
+
+
+
+//***********************JSON Crash Course **********
+// var person = {
+//     name:'john',
+//     age: 39,
+//     adress: {
+//         street: "lawinowa",
+//         house: 34,
+//     },
+//     colors: ["red","yellow"]
+// }
+
+// person = JSON.stringify(person); // - to json ""
+// person = JSON.parse(person); // - to object
+
+// console.log(person);
+
+
+
+//***********************FETCH******************
+// document.querySelector('#fetch').addEventListener('click', loadJson);
+// function loadJson(){
+//     fetch('data/medtasks.json')
+//         .then(function(res){
+//             return res.json();
+//         })
+//         .then(function(data){
+//             console.log(data[0].answer1);
+//             console.log(data);
+//             const firstTry = data.filter(z => z.id ==="rat");
+//             console.log(firstTry);
+//             // const ratunkowa = JSON.parse(this.responseText);
+//             // const secondTry = ratunkowa.filter(z => z.id ==="rat");
+//             // console.log(secondTry);
+//         });
+// }
+
+
+
+//************************WORKING WITH EXTERNAL API
+document.querySelector('#loadJokes').addEventListener('click', loadJokes);
+
+function loadJokes(){
+    const xhr = new XMLHttpRequest();
+    const number = document.querySelector('#jokesNumber').value;
+    xhr.open('GET',	
+    `http://api.icndb.com/jokes/random/${number}`, true);        
+    
+    xhr.onload = function(){
+        if(this.status ===200){
+            const response = JSON.parse(this.responseText);
+
+            let output = '';
+
+            if(response.type === 'success'){
+
+                response.value.forEach(function(joke){
+                    output += `<br><br>${joke.joke}`;
+                });   
+            }
+
+            else{
+                console.log('something went wrong')
+            }
+         
+            document.querySelector("#jokes").innerHTML = output;
         }
     }
+
+    xhr.send(); 
+    
 }
-
-
-//JSON Crash Course *********************************
-
-var person = {
-    name:'john',
-    age: 39,
-    adress: {
-        street: "lawinowa",
-        house: 34,
-    },
-    colors: ["red","yellow"]
-}
-
-person = JSON.stringify(person); // - to json ""
-person = JSON.parse(person); // - to object
-
-console.log(person);
-
-
-
-//***********************FETCH
-document.querySelector('#fetch').addEventListener('click', loadJson);
-function loadJson(){
-    fetch('data/medtasks.json')
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data){
-            console.log(data[0].answer1);
-            console.log(data);
-            const firstTry = data.filter(z => z.id ==="rat");
-            console.log(firstTry);
-            // const ratunkowa = JSON.parse(this.responseText);
-            // const secondTry = ratunkowa.filter(z => z.id ==="rat");
-            // console.log(secondTry);
-        });
-}
-
